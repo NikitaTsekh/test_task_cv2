9abd7ccb958c99f6c4d309dc457f33b05f00a0ab
nceh1988 <nceh1988@gmail.com>
2023-01-04 16:08:02 +0300

    First commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+This is solution for trainee task done by Varakin https://www.youtube.com/@varakin
+I fixed few details. It was tested at Ubuntu 20.04
+
---
+++ main.py
@@ -0,0 +1,40 @@
+import cv2
+
+# HSV
+low_black = (37,37,37)
+high_black = (0,0,0)
+
+
+
+
+
+
+cap = cv2.VideoCapture("/home/nikita/PycharmProjects/test_task_cv/v.mp4")
+
+width = int(cap.get(3))
+height= int(cap.get(4))
+result_frames = []
+
+#
+while True:
+    ret, frame =cap.read()
+    if not ret:
+        break
+
+    frame_b = cv2.inRange(frame, high_black, low_black)
+    contours, hierarchy = cv2.findContours(frame_b.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame,contours,-1,(255,8,0),cv2.FILLED)
+
+    result_frames.append(frame)
+
+cap.release()
+
+#
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+# fourcc = cv2.VideoWriter_fourcc(*'avc1')
+video = cv2.VideoWriter('/home/nikita/PycharmProjects/test_task_cv/video.mp4',fourcc, 20.0, (width,height))
+#
+#
+for result_frame in result_frames:
+
+    video.write(result_frame)
